2a0cb64789035f810bfa0658e87c982eb39c441e
milenaAristiza <141691793+milenaAristiza@users.noreply.github.com>
2023-08-28 18:04:46 -0500

    changes plataforma

--- gps-control/src/routes/Plataforma/plataforma.js
+++ gps-control/src/routes/Plataforma/plataforma.js
@@ -2,6 +2,7 @@ import Group71 from "../../assets/Group71.png";
 import Group73 from "../../assets/Group73.svg";
 import Frame3 from "../../assets/Frame3.png";
 import Frame4 from "../../assets/Frame4.png";
+import flecha from "../../assets/FLECHA_SELECT.svg";
 import "./plataforma.css"
 export default function Plataforma() {
 
@@ -9,15 +10,15 @@ export default function Plataforma() {
 
         <div className="platform__title">
             <h1>¿Buscas un rastreador gps para vehículos?</h1>
-            <h2>TENEMOS MÁS QUE ESO!</h2>
+            <h2>¡TENEMOS MÁS QUE ESO!</h2>
         </div>
         <div className="platform__content">
             <div className="content__info-platform">
                 <div className="info__platform-container-text">
+                    <div className="info__platform-container-back"></div>
                     <p>Controla de forma integral tu operación logística, monitorea en tiempo real tu flota, ahorra tiempos de gestión y reduce costos.</p>
                 </div>
-                <img src={Group73} alt="camiones" />
-                <img src={Group71} alt="camionesdos"></img>
+                <img src={Group71} className="vehiculo" alt="camionesdos"></img>
             </div>
             <div className="content__form-platform">
                 <form>
@@ -30,19 +31,21 @@ export default function Plataforma() {
                             <select>
                                 <option>Departamento</option>
                             </select>
+                            <img src={flecha} />
                         </div>
                         <div className="content__form-texarea">
                             <textarea rows="5" cols="33" placeholder="Describe tu solicitud">
                             </textarea>
                         </div>
                         <div className="content__button-form">
-                            <img src={Frame3} alt="b"></img>
+                            <img src={Frame3} className="img-bottom" alt="b"></img>
                             <p>www.gpscontrol.co</p>
                             <button>Enviar</button>
                         </div>
                     </div>
                 </form>
-                <img src={Frame4} alt="a"></img>
+                <img src={Group73} className="img_bg_left" alt="camiones" />
+                <img src={Frame4} className="gato" alt="a"></img>
             </div>
         </div>
 
