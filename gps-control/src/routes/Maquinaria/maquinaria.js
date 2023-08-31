97af3223b165e0a7f02fdcd058b572765b520ae4
milenaAristiza <141691793+milenaAristiza@users.noreply.github.com>
2023-08-31 17:42:57 -0500

    responsive maquinaria

--- gps-control/src/routes/Maquinaria/maquinaria.js
+++ gps-control/src/routes/Maquinaria/maquinaria.js
@@ -9,32 +9,32 @@ import "./maquinaria.css"
 import "../../components/Form/Form.css"
 export default function Plataforma() {
     return <section className="maquinaria">
-        <div className="title__maquinaria">
-        <h1>¡QUE NO INMOVILICEN TU MÁQUINA AMARILLA!</h1>
 
+        <div className="title__maquinaria">
+            <h1>¡QUE NO INMOVILICEN TU MÁQUINA AMARILLA!</h1>
         </div>
         <div className="container__elements-maquinaria">
-        <div className="container__info">
+            <div className="container__info">
                 <img className="container__info-img1" src={excavator}></img>
                 <img src={heavyExcavator}></img>
-                </div>
+            </div>
             <div className="container1__elemnts">
-            <img className="container__elements-img" src={Vector8}></img>
-            <div className="info__maquinaria-container-text">
+                <img className="container__elements-img" src={Vector8}></img>
+                <div className="info__maquinaria-container-text">
                     <div className="info__maquinaria-container-back"></div>
                     <p>toda Maquinaria Amarilla debe contar por ley con un GPS. #GPSControl SOMOS PROVEEDORES AUTORIZADOS por la Policía Nacional de Colombia para la prestación de SERVICIOS DE POSICIONAMIENTO GLOBAL (GPS) para Maquinaria Amarilla.</p>
                     <div className="mixb"></div>
                     <img className="imgCont" src={Group73}></img>
                 </div>
-                <img className="vehiculo" src={excavadora2}></img>
+                
             </div>
-            <div className="container__form">
-            <Form></Form>
-            <img src={Frame4} className="gato" alt="a"></img>
-
+            <img className="vehiculo" src={excavadora2}></img>
+            <div className="cont1form">
+                <div className="container__form">
+                    <Form></Form>
+                </div>
             </div>
-
         </div>
-
+        <img src={Frame4} className="gato" alt="a"></img>
     </section>
 }
