e0e24e144bd8a36307804fe652f4121ba455508d
Dev-Alex-H <henaochacon@gmail.com>
2023-12-15 10:21:51 -0500

    add some styles

--- gps-control/src/components/Section2_monitoreo/Section2_monitoreo.js
+++ gps-control/src/components/Section2_monitoreo/Section2_monitoreo.js
@@ -7,7 +7,7 @@ function Section2_monitoreo({ data }) {
         setText(data);
     }, [data]);
     return <>
-        <div className="sectionNew1--cont1">
+        <div className="sectionNew1--cont1 sectionNew1--cont12">
             <div className="margin--container2">
                 <h2 className="title--component__monitoreo">Cumplir con los siguientes requisitos generales:</h2>
                 <p className="subTitle--component__monitoreo1">{text}</p>
